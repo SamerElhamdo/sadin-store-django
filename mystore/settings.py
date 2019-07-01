65d951a5aadb4abe50f625721deac9f95e857fda
samer elhamdo <samer.elhamdoo@gmail.com>
2019-07-01 04:31:29 +0300

    change database

--- mystore/settings.py
+++ mystore/settings.py
@@ -13,6 +13,7 @@ import django_heroku
 import os
 
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -89,9 +90,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-import dj_database_url 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+
 
 
 # Password validation
@@ -145,3 +144,7 @@ LOGIN_REDIRECT_URL = '/control/home'
 LOGIN_URL = '/control/login'
 
 django_heroku.settings(locals)
+
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
